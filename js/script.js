042a4e9684b28ab636bc29fb336641fe708bfbf4
Bradley Thompson <31293826+Bisrad@users.noreply.github.com>
2020-09-19 01:01:16 -0700

    Update JS to view api data in html

--- js/script.js
+++ js/script.js
@@ -1,44 +1,73 @@
+// document load
+$(function() {
+    console.log( "ready!" );
+});
+
 // declare variables 
+var search = document.querySelector('.inputText')
 var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var icon = document.querySelector('.icon')
-var city = document.querySelector('.name')
-var temp = document.querySelector('.desc')
-var desc = document.querySelector('.temp')
+var iconSet = document.querySelector('.icon')
+var main = document.querySelector('#name')
+var temp = document.querySelector('.temp')
+var desc = document.querySelector('.desc')
+
+
+// localStorage setup 
+// Finish Local Storage setup
+      
+    let saveUserInput = '';  
+    let userValue = '';
+
+    localStorage.setItem("City", saveUserInput)
+    console.log(userValue);
 
 // OpenWeather API
 
-// on click function
-button.addEventListener('click', function() {
-    // add catch to api for user input feild 
-    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid64bf7a1719397bac60c5d6a0b39a4a3d')
+button.addEventListener("click", function() {
+    console.log(search.value); // on click check
+    }, false);
 
-        // call api and return json promises
-        .then(response => response.json())
-        .then(data => {
+    // fix to add user input '+search.value+'
+    // expand to have 4 day forcast 
 
-        // check api response
-        console.log(response)    
-        
-        // get data to fill html blocks
-          var iconEle = data['cod'];
-          var cityValue = data['name'];
-          var tempValue = data['main']['temp'];
-          var descValue = data['weather'][0]['description'];
-          
-        // fill html area with api info  
-        icon.innerHTML = iconEle; 
-        city.innerHTML = cityValue;
-        temp.innerHTML = tempValue;
-        desc.innerHTML = descValue;
-        
-        })
+    // let request = new XMLHttpRequest(); // look into this load request
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=phoenix&units=metric&appid=64bf7a1719397bac60c5d6a0b39a4a3d')
 
-        // change heading to match city input
-        // document.getElementById("heading").value = inputValue.value;
-        
-        // catch error if no value returned 
-        .catch(err => alert("No city entered, or misspelled. Please try again."))
-        })
+    // call api and return json promises
+    .then(response => response.json())
+    .then(data => {
+    // check data pull
+    console.log(data)
+    // fill variables with data
+    var tempValue = data['main']['temp'];
+    var nameValue = data['name'];
+    var descValue = data['weather'][0]['description'];
+    // replace html
+    main.innerHTML = nameValue;
+    desc.innerHTML = descValue;
+    temp.innerHTML = Math.round(tempValue)+' &degF';
+   
+    // pull icons from openweather
+    $(".icon").html("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+    })  
 
-        
+    //error catch
+    .catch(err => {
+        console.log("Error Reading data " + err);
+});
+
+// --------------------------------------------------------------------
+
+// set browser to open with last searched city's data using localstorage memory
+
+// create an onclick function which clears localstorage (clear history)
+  $("#clearHistory").on("click", () => {
+    localStorage.clear();
+  });
+
+// need to declare a variable for recent search
+  let recentSearchOption = "";
+
+// need to give newly created recent searches onclick function
+//   $("#recentSearchOption").on("click", () => {});
+// });
